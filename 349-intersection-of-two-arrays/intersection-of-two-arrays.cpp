5964dac29ece48658c0c73f463a117af162e0ecf
shivam16maurya10-cyber <shivam16maurya10@gmail.com>
2026-02-14 00:34:43 +0530

    Time: 1 ms (46.90%) | Memory: 14 MB (77.36%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        
+        for(int i = 0; i < nums1.size(); i++) {
+            int element = nums1[i];
+            
+            for(int j = 0; j < nums2.size(); j++) {
+                if(element == nums2[j]) {
+                    // Check if element already exists in answer to avoid duplicates
+                    bool alreadyExists = false;
+                    for(int k = 0; k < ans.size(); k++) {
+                        if(ans[k] == element) {
+                            alreadyExists = true;
+                            break;
+                        }
+                    }
+                    
+                    if(!alreadyExists) {
+                        ans.push_back(element);
+                    }
+                    break; // Found match, no need to continue inner loop
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
